ce74086dc9c183e65bc981a2ad0703ff15bb41c8
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-10 17:20:15 +0530

    Module 2

---
+++ Variables_and_Data_Types.py
@@ -0,0 +1,24 @@
+name = "Arman"
+age = 20
+height = 5.8
+student = True
+result = None
+number = 3 + 4j
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(height)
+print(type(height))
+
+print(student)
+print(type(student))
+
+print(result)
+print(type(result))
+
+print(number)
+print(type(number))
