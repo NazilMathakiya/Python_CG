d271dd9666a39b1e8b48b76a4b56a34374b457fc
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-10 11:45:52 +0530

    Add Python practice

---
+++ practice.py
@@ -0,0 +1,33 @@
+# print("Hello, World!")
+# print("I am learning Python.")
+# print("Python will help me learn DSA.")
+##
+
+# name = "Arman"
+# age = 20
+
+# print(name)
+# print(age)
+
+##
+
+# a = 10
+# b = 20
+
+# print(a + b)
+
+# import math;
+# print(math.sqrt(49));
+
+# name =  "Nazil";
+# age = 18;
+# print(name);
+# print(age);
+
+name = "Arman"
+age = 20
+marks = 85
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
