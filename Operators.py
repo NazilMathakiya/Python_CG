2a808cd0ae457201c5cce2239b5214a5e696f175
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-11 09:34:40 +0530

    Python practice

---
+++ Operators.py
@@ -0,0 +1,37 @@
+# Arithmetic Operators
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# Assignment Operators
+x = 10
+
+x += 5
+print(x)   # 15
+
+x -= 3
+print(x)   # 12
+
+x *= 2
+print(x)   # 24
+
+x /= 4
+print(x)   # 6.0
+
+# Comparison Operators
+a = 10
+b = 20
+
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
