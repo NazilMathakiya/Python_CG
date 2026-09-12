b01c0b1ada99bdf303214aa93307aeb9b1abb6e5
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-12 10:01:38 +0530

    Loop

---
+++ Loops.py
@@ -0,0 +1,20 @@
+# For Loop
+for i in range(5):
+    print(i)
+
+
+# Range
+# range(start, stop, step)
+for i in range(1, 6):
+    print(i)
+
+# Step
+for i in range(2, 11, 2):
+    print(i)
+
+# Nested-Loop
+for i in range(3):
+    for j in range(2):
+        print(i, j)
+
+# 
