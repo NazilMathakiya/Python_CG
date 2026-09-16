abb755c9e859eb86a7e1f9d790540e92e9e8aabe
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-16 10:40:27 +0530

    set practice

---
+++ Sets.py
@@ -0,0 +1,13 @@
+# A set is an unordered collection of unique elements.
+
+numbers = {10, 20, 30, 20}
+print(numbers)  #{10, 20, 30}
+
+
+# creating a set
+s = {1, 2, 3}
+
+# empty set
+s = set()
+# {} creates an empty dictionary, not a set.
+
