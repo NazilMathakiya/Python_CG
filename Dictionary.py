a82ccec56d3d062bd267048303cf542cbbc8d676
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-19 16:41:45 +0530

    Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -21,3 +21,51 @@ student.popitem()
 
 # Remove everything:
 student.clear()
+
+
+student = {"name": "Arman", "age": 20}
+
+print("name" in student)   #true
+print("marks" in student)  #false
+
+
+# traversing
+for key in student:
+    print(key)
+
+for value in student.values():
+    print(value)
+
+for key, value in student.items():
+    print(key, value)
+
+
+# imp method
+student = {"name": "Arman", "age": 20}
+
+print(student.keys())
+print(student.values())
+print(student.items())
+
+print(student.get("name"))
+
+student.update({"marks": 90})
+print(student)
+
+student.pop("age")
+print(student)
+
+
+# nested dict
+students = {
+    "student1": {
+        "name": "Arman",
+        "marks": 85
+    },
+    "student2": {
+        "name": "Rahul",
+        "marks": 90
+    }
+}
+
+print(students["student1"]["marks"])
