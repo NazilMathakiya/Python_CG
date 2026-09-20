9b08664d558b42dae0a5d2804614dfddd3c81669
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-20 17:17:14 +0530

    comprehensions

--- Comprehensions.py
+++ Comprehensions.py
@@ -3,14 +3,12 @@ squares = []
 
 for i in range(1, 6):
     squares.append(i * i)
-
 print(squares)
 
 
 
 # Using comprehensions
 squares = [i * i for i in range(1, 6)]
-
 print(squares)
 
 
@@ -20,7 +18,11 @@ print(squares)
 
 # with condition find even number
 even = [i for i in range(1, 11) if i % 2 == 0]
-
 print(even)
+
+
+# if else
+result = ["Even" if i % 2 == 0 else "Odd" for i in range(1, 6)]
+print(result)
 
 
