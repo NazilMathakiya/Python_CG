b079abcc448eb40f6a480e91e4ca382b9e91538c
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-20 17:24:12 +0530

    comprehensions

--- Comprehensions.py
+++ Comprehensions.py
@@ -26,3 +26,6 @@ result = ["Even" if i % 2 == 0 else "Odd" for i in range(1, 6)]
 print(result)    #['Odd', 'Even', 'Odd', 'Even', 'Odd']
 
 
+# set comprehension
+squares = {i * i for i in range(1, 6)}
+print(squares)
