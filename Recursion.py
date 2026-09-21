1c7df672ecbeadf2ea5f06bc256c5b762a6b099e
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-21 11:42:56 +0530

    Recursion

--- Recursion.py
+++ Recursion.py
@@ -1,3 +1,4 @@
+# basic recursion
 def count(n):
     if n == 0:
         return
@@ -8,3 +9,23 @@ def count(n):
 count(5)
 
 
+
+# Factorial
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+print(factorial(5))
+
+
+
+# sum of number
+def total(n):
+    if n == 0:
+        return 0
+
+    return n + total(n - 1)
+
+print(total(5))
